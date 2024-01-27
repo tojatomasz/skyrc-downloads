90e3d190d75652b09d2087981181245eda29bb2b
tojatomasz <maczseze@gmail.com>
2024-01-27 22:31:58 +0100

    Update downloads-scrapper-skyrc.py
    - Added minimum file index
    - Added min/max values into file name
    - Added skyrc into filename
    - Replace %20 with space in filename

--- downloads-scrapper-skyrc.py
+++ downloads-scrapper-skyrc.py
@@ -1,10 +1,11 @@
 import requests
 from urllib.parse import urljoin
 
+min_file_number = int(input("Enter the starting file number to check: "))
 max_file_number = int(input("Enter the maximum file number to check: "))
 
 base_url = "https://www.skyrc.com/files/"
-output_file = "available_files.txt"
+output_file = f"skyrc-available_files-{min_file_number}-{max_file_number}.txt"
 
 def get_final_url_after_redirects(url):
     with requests.get(url, stream=True) as response:
@@ -13,12 +14,13 @@ def get_final_url_after_redirects(url):
 print("Starting the URL checking process...")
 
 with open(output_file, "w") as file:
-    for i in range(1, max_file_number + 1):
+    for i in range(min_file_number, max_file_number + 1):
         url = urljoin(base_url, str(i))
         print(f"Checking URL: {url}")
         try:
             final_url = get_final_url_after_redirects(url)
             filename = final_url.split('/')[-1]  # Extracts the file name from the URL
+            filename = filename.replace("%20", " ") # Replace %20 with space
             if filename == "not_found":
                 print("File not found!")
                 continue
